14e98d3779643e81d01d987dec732a1f83b596ab
junbly <junhyeon47@gmail.com>
2019-07-04 22:26:38 +0900

    #2-1. Creating React Components with JSX

--- src/Movie.js
+++ src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
     render() {
         return (
             <div>
-                <MoviePoster />
-                <h1>hello this is a movie</h1>
+                <MoviePoster poster={this.props.poster} />
+                <h1>{this.props.title}</h1>
             </div>
         );
     };
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
     render() {
         return (
-            <img src="https://m.media-amazon.com/images/M/MV5BMTM0NjQ4OTgyNV5BMl5BanBnXkFtZTcwOTU2MzQ4Nw@@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg"/>
+            <img src={this.props.poster}/>
         );
     }
 }
