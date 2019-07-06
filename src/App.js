5d5f1b4b09741f3cfde8dc2490555c7bfff405d6
junbly <junhyeon47@gmail.com>
2019-07-06 14:37:31 +0900

    #4-2. Practicing this.setState()

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
- 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title: "Oldbody",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Aladin",
-    poster: "https://upload.wikimedia.org/wikipedia/en/9/9a/Aladdin_%28Official_2019_Film_Poster%29.png"
-  }
-];
 
 class App extends Component {
   
   state = {
-    greeting: 'Hello'
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+      },
+      {
+        title: "Oldbody",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+      },
+      {
+        title: "Aladin",
+        poster: "https://upload.wikimedia.org/wikipedia/en/9/9a/Aladdin_%28Official_2019_Film_Poster%29.png"
+      }
+    ]
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          ...this.state.movies, // 기존의 영화 목록을 그대로 가져옴
+          {
+            title: "악인전",
+            poster: "http://img.khan.co.kr/news/2019/05/06/l_2019050601000595800041521.jpg"
+          }
+        ]
       })
     }, 2000);
   }
@@ -38,8 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
       </div>
