fd90e02ec2ca0b32a4bea7ee511772494c078df9
junbly <junhyeon47@gmail.com>
2019-07-09 21:10:42 +0900

    #6-1. Ajax in React

--- src/App.js
+++ src/App.js
@@ -3,33 +3,10 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-  
-  state = {
-  }
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-              title: "Matrix",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
-          },
-          {
-              title: "Full Metal Jacket",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-          },
-          {
-              title: "Oldbody",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-          },
-          {
-              title: "Aladin",
-              poster: "https://upload.wikimedia.org/wikipedia/en/9/9a/Aladdin_%28Official_2019_Film_Poster%29.png"
-          }
-      ]
-      })
-    }, 2000);
+    console.log(fetch(`https://yts.lt/api/v2/list_movies.json?sort_by=rating`));
   }
 
   _rednerMovies = () => {
